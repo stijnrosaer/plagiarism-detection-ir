c897495c0c92f917ee1b3434a3a256edce448f57
Quentindh <quentindehaes@outlook.com>
2021-01-25 16:37:45 +0100

    added jaccard.py comments

--- jaccard.py
+++ jaccard.py
@@ -5,32 +5,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-OVERWRITE = False
-DEBUG = False
+OVERWRITE = False #used to state that the index of jacard values needs to be overwritten
+DEBUG = False #used to print extra info
 
 def jaccard_index(query, document):
+    """
+    calculate the jacard value between the 2 documents according to the algorithm
+    :param query: the first document
+    :param document: the second document
+    :return: the jacard value
+    """
     intersection = set(query).intersection(set(document))
     union = set(query).union(set(document))
     return len(intersection) / len(union)
 
 
 def jaccard_df(df, filename):
-    if os.path.isfile("jaccard-" + filename) and not OVERWRITE:
+    """
+    either read or generate the jacard index and return the similarity between all different documents
+    :param df: the collection of documents
+    :param filename: the name used to determine the location where the index exists or will be written
+    :return:the jacard values of all document combos
+    """
+    if os.path.isfile("jaccard-" + filename) and not OVERWRITE: #check wether or not we can use an existing index
         print("Using existing jaccard-library")
         df2 = pd.read_csv("jaccard-" + filename)
     else:
         print(f"Building jaccard-library for {filename}")
 
         scores = []
-        for pair in itertools.combinations(df.to_records(index=False), 2):
-            if pair[0]["News_ID"] != pair[1]["News_ID"]:
-                jaccard_score = jaccard_index(pair[0]["article"].split(), pair[1]["article"].split())
-                scores.append([pair[0]["News_ID"], pair[1]["News_ID"], jaccard_score])
+        for pair in itertools.combinations(df.to_records(index=False), 2): #iterate over every pair of 2 documents in the collection
+            if pair[0]["News_ID"] != pair[1]["News_ID"]: #check that you don't compare a document with itself
+                jaccard_score = jaccard_index(pair[0]["article"].split(), pair[1]["article"].split()) #calculate the jacard value for the pair of documents
+                scores.append([pair[0]["News_ID"], pair[1]["News_ID"], jaccard_score]) #store this score in the list
 
         df2 = pd.DataFrame(scores, columns=["id1", "id2", "score"])
 
 
-        df2.to_csv("jaccard-" + filename, index=False)
+        df2.to_csv("jaccard-" + filename, index=False) #write all scores to a file to no longer require calculations when redoing this function
 
         if DEBUG:
             print(len(scores))
