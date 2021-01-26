e34025c402a5ef7a9b0ca13e407b9ebd864d04ad
Quentindh <quentindehaes@outlook.com>
2021-01-26 11:23:04 +0100

    added shingles to jacard

--- jaccard.py
+++ jaccard.py
@@ -3,10 +3,10 @@ import itertools
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
+from nltk import ngrams
 
 OVERWRITE = False #used to state that the index of jacard values needs to be overwritten
-DEBUG = False #used to print extra info
+DEBUG = True #used to print extra info
 
 def jaccard_index(query, document):
     """
@@ -20,9 +20,10 @@ def jaccard_index(query, document):
     return len(intersection) / len(union)
 
 
-def jaccard_df(df, filename):
+def jaccard_df(df, filename, shingles=2):
     """
     either read or generate the jacard index and return the similarity between all different documents
+    :param shingles: the length of the shingles
     :param df: the collection of documents
     :param filename: the name used to determine the location where the index exists or will be written
     :return:the jacard values of all document combos
@@ -36,7 +37,9 @@ def jaccard_df(df, filename):
         scores = []
         for pair in itertools.combinations(df.to_records(index=False), 2): #iterate over every pair of 2 documents in the collection
             if pair[0]["News_ID"] != pair[1]["News_ID"]: #check that you don't compare a document with itself
-                jaccard_score = jaccard_index(pair[0]["article"].split(), pair[1]["article"].split()) #calculate the jacard value for the pair of documents
+                # jaccard_score = jaccard_index(pair[0]["article"].split(), pair[1]["article"].split()) #calculate the jacard value for the pair of documents
+                jaccard_score = jaccard_index(list(ngrams(pair[0]["article"].split(), shingles)), list(ngrams(pair[1]["article"].split(),shingles))) #calculate the jacard value for the pair of documents
+
                 scores.append([pair[0]["News_ID"], pair[1]["News_ID"], jaccard_score]) #store this score in the list
 
         df2 = pd.DataFrame(scores, columns=["id1", "id2", "score"])
