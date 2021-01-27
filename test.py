18fb7c62a46949cba22958b555d14cdc599e31c6
jdepreter <jolan.depreter@gmail.com>
2021-01-27 11:25:12 +0100

    added comments to tests

--- test.py
+++ test.py
@@ -37,12 +37,14 @@ class TestIdealIterRowsBands(unittest.TestCase):
         self.assertEqual(rows, 1)
 
     def test_find_correct_bands(self):
+        # These are the values from slide 52, check if we find the corresponding bands
         iter, bands, rows = get_ideal_bands(0.5, 0.8, 0.470, 0.99, 100, 100)
         self.assertEqual(iter, 100)
         self.assertEqual(bands, 20)
         self.assertEqual(rows, 5)
 
     def test_find_correct_iter_and_bands(self):
+        # These are the values from slide 52, check if we find the corresponding iterations and bands
         iter, bands, rows = get_ideal_bands(0.5, 0.8, 0.470, 0.99, 60, 200)
         self.assertEqual(iter, 100)
         self.assertEqual(bands, 20)
@@ -51,6 +53,7 @@ class TestIdealIterRowsBands(unittest.TestCase):
 
     def test_cursus_values(self):
         # test the probability calculations used by get_ideal_bands
+        # values from slide 52
         bands = 20
         rows = 5
         self.assertAlmostEqual(calc_prob_sim(0.2, bands, rows), 0.006, 3)
@@ -65,6 +68,7 @@ class TestIdealIterRowsBands(unittest.TestCase):
 class TestLSH(unittest.TestCase):
 
     def test_all_candidate_pairs(self):
+        # all signatures are equal 
         signature_matrix = [
             [(1,1), (2,1), (3,1), (4,1), (5,1)],
             [(1,1), (2,1), (3,1), (4,1), (5,1)],
@@ -76,6 +80,7 @@ class TestLSH(unittest.TestCase):
         self.assertListEqual(result, [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5), (3, 4), (3, 5), (4, 5)])
 
     def test_no_candidate_pair(self):
+        # all signatures are different
         signature_matrix = [
             [(1,1), (2,2), (3,3), (4,4), (5,5)],
             [(1,1), (2,2), (3,3), (4,4), (5,5)],
@@ -87,6 +92,8 @@ class TestLSH(unittest.TestCase):
         self.assertListEqual(result, [])
 
     def test_one_candidate_pair(self):
+        # one minhash value is equal
+        # candidate pair depends on size of bands
         signature_matrix = [
             [(1,1), (2,2), (3,3), (4,4), (5,1)],
             [(1,1), (2,2), (3,3), (4,4), (5,5)],
