4aea7572890575ba2345b2b10a6f3f9c89f5344f
jdepreter <jolan.depreter@gmail.com>
2021-01-27 16:19:52 +0100

    images and small update to plotter

--- plotter.py
+++ plotter.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-bands = 3
-rows = 8
+bands = 4
+rows = 6
 
 x = np.arange(0, 1, 0.01)
 plt.plot(x, 1-pow(1-pow(x,rows), bands))
 plt.title(f"bands: {bands}, rows: {rows}")
+plt.xlabel("doc sim")
+plt.ylabel("chance they are candidate pairs")
 
 # plt.show()
 plt.savefig(f"{bands}-{rows}.png")
